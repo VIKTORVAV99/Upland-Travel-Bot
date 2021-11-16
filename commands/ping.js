645568e0c47ef40b3ec20c926b561b581e709973
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2021-11-17 00:12:11 +0100

    Updated ping with latency and embed

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+  data: new SlashCommandBuilder().setName('ping').setDescription('Check the latency of the bot'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    const embedResponse = {
+      title: 'Pong!',
+      fields: [
+        { name: 'Bot latency', value: `${Date.now() - interaction.createdTimestamp} ms`, inline: true },
+        { name: 'API latency', value: `${interaction.client.ws.ping} ms`, inline: true },
+      ],
+    };
+    await interaction.reply({ embeds: [embedResponse] });
   },
 };
