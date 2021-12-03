b71e70a80f0b11759b63a6a3a9e2e6a3ce2a058a
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2021-12-04 00:09:18 +0100

    Migrate to ESM

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,13 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+import { SlashCommandBuilder } from '@discordjs/builders';
 
-module.exports = {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Check the latency of the bot'),
-  async execute(interaction) {
-    const embedResponse = {
-      title: 'Pong!',
-      fields: [
-        { name: 'Bot latency', value: `${Date.now() - interaction.createdTimestamp} ms`, inline: true },
-        { name: 'API latency', value: `${interaction.client.ws.ping} ms`, inline: true },
-      ],
-    };
-    await interaction.reply({ embeds: [embedResponse] });
-  },
-};
+export const data = new SlashCommandBuilder().setName('ping').setDescription('Check the latency of the bot');
+export async function execute(interaction) {
+  const embedResponse = {
+    title: 'Pong!',
+    fields: [
+      { name: 'Bot latency', value: `${Date.now() - interaction.createdTimestamp} ms`, inline: true },
+      { name: 'API latency', value: `${interaction.client.ws.ping} ms`, inline: true },
+    ],
+  };
+  await interaction.reply({ embeds: [embedResponse] });
+}
--- commands/travel.js
+++ commands/travel.js
@@ -1,42 +1,40 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { travelToFrom } = require('../travel-logic');
-const { cities } = require('../data/cities.json');
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { travelToFrom } from '../travel-logic.js';
+import jsonData from '../data/cities.json';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('travel')
-    .setDescription('Gives you the best route from point A to point B based on the selected method.')
-    .addStringOption((option) =>
-      option
-        .setName('from')
-        .setDescription('Enter the destination you are traveling from.')
-        .setRequired(true)
-        .addChoices(cities.sort())
-    )
-    .addStringOption((option) =>
-      option
-        .setName('to')
-        .setDescription('Enter the destination you are traveling to.')
-        .setRequired(true)
-        .addChoices(cities.sort())
-    )
-    .addStringOption((option) =>
-      option
-        .setName('method')
-        .setDescription('The method used to find the route.')
-        .addChoice('cheapest', 'cheapest')
-        .addChoice('fastest', 'fastest')
-        .addChoice('simplest', 'simplest')
-    ),
-  async execute(interaction) {
-    const from = interaction.options.getString('from');
-    const to = interaction.options.getString('to');
-    const method = interaction.options.getString('method');
+export const data = new SlashCommandBuilder()
+  .setName('travel')
+  .setDescription('Gives you the best route from point A to point B based on the selected method.')
+  .addStringOption((option) =>
+    option
+      .setName('from')
+      .setDescription('Enter the destination you are traveling from.')
+      .setRequired(true)
+      .addChoices(jsonData.cities.sort())
+  )
+  .addStringOption((option) =>
+    option
+      .setName('to')
+      .setDescription('Enter the destination you are traveling to.')
+      .setRequired(true)
+      .addChoices(jsonData.cities.sort())
+  )
+  .addStringOption((option) =>
+    option
+      .setName('method')
+      .setDescription('The method used to find the route.')
+      .addChoice('cheapest', 'cheapest')
+      .addChoice('fastest', 'fastest')
+      .addChoice('simplest', 'simplest')
+  );
+export async function execute(interaction) {
+  const from = interaction.options.getString('from');
+  const to = interaction.options.getString('to');
+  const method = interaction.options.getString('method');
 
-    if (from != to) {
-      await interaction.reply({ embeds: [travelToFrom(from, to, method)] });
-    } else {
-      await interaction.reply('You are already at the destination!');
-    }
-  },
-};
+  if (from != to) {
+    await interaction.reply({ embeds: [travelToFrom(from, to, method)] });
+  } else {
+    await interaction.reply('You are already at the destination!');
+  }
+}
