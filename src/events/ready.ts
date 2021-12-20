cdcdaf6a102b3ecdb48166ecfc2acbe31236b34f
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2021-12-20 22:30:22 +0100

    prettier formating

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,6 @@ import type { Client } from 'discord.js';
 
 export const name = 'ready';
 export const once = true;
-export function execute(client: Client): void  {
+export function execute(client: Client): void {
   console.log(`Ready!\nLogged in as ${client.user?.tag}`);
 }
