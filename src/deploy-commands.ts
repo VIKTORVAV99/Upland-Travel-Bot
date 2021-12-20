ce1e67eda68e1be50ad05636108b4fef83447168
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2021-12-21 00:12:23 +0100

    Clean up `deploy-commands.ts`

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,30 +1,24 @@
+import 'dotenv/config';
 import { readdirSync } from 'fs';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { Command } from './interfaces/command';
+import type { Command } from './interfaces/command';
 
 const commands: unknown[] = [];
 const commandFiles = readdirSync('dist/commands').filter((file) => file.endsWith('.js'));
 
-async function readCommandFiles() {
-  for (const file of commandFiles) {
-    commands.push(
-      await import(`./commands/${file}`).then((command: Command) => {
-        console.log(`Added command: ${command.data.name}`);
-        return command.data.toJSON();
-      })
-    );
-  }
+for (const file of commandFiles) {
+  commands.push(
+    await import(`./commands/${file}`).then((command: Command) => {
+      console.log(`Added command: ${command.data.name}`);
+      return command.data.toJSON();
+    })
+  );
 }
 
-async function deployCommands() {
-  await readCommandFiles();
-  const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN ?? '');
-  console.log(`Registering ${commands.length} commands...`);
-  rest
-    .put(Routes.applicationCommands(process.env.CLIENT_ID ?? ''), { body: commands })
-    .then(() => console.log(`Successfully registered ${commands.length} application commands.`))
-    .catch(console.error);
-}
-
-deployCommands();
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN ?? '');
+console.log(`Registering ${commands.length} commands...`);
+rest
+  .put(Routes.applicationCommands(process.env.CLIENT_ID ?? ''), { body: commands })
+  .then(() => console.log(`Successfully registered ${commands.length} application commands.`))
+  .catch(console.error);
