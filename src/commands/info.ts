901a0f418d1fcaf9d27a00ca6d811d30fa833c9d
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2022-01-03 20:41:53 +0100

    changed the command `/status` to `/info` and added more fields.

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,22 +1,28 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
+import { hyperlink, SlashCommandBuilder, userMention } from '@discordjs/builders';
 import { CommandInteraction, MessageEmbedOptions } from 'discord.js';
 import { convertMs } from '../utils/msToLongTime.js';
 import { plural } from '../utils/plural.js';
 
 export const data = new SlashCommandBuilder()
-  .setName('status')
+  .setName('info')
   .setDescription('Gives the status information about the bot.');
 
 /** The main function that executes the command. */
 export async function execute(interaction: CommandInteraction) {
+  const botUsername: string = interaction.client.user?.toString() ?? 'Error';
+  const creatorUsername: string = userMention('403508484760666112');
   const amountOfServers: string = interaction.client.guilds.cache.size.toString();
   const uptime = convertMs(interaction.client.uptime ?? 0);
   const readyAt: string = interaction.client.readyAt?.toUTCString() ?? 'Error';
   const createdAt: string = interaction.createdAt.toUTCString();
+  const botRepo: string = hyperlink('GitHub', 'https://github.com/VIKTORVAV99/Upland-Travel-Bot');
   const embedResponse: MessageEmbedOptions = {
-    title: 'Bot Status',
-    description: 'Basic status information about the bot.',
+    title: 'Bot info',
+    description: 'Basic information about the bot.',
     fields: [
+      { name: 'Username', value: `Signed in as ${botUsername}`, inline: true },
+      { name: 'Creator', value: `Created by ${creatorUsername}`, inline: true },
+      { name: 'Repository', value: botRepo, inline: true },
       {
         name: 'Servers',
         value: `The bot is in ${amountOfServers} ${plural(
@@ -48,4 +54,4 @@ export async function execute(interaction: CommandInteraction) {
   };
   await interaction.reply({ embeds: [embedResponse] });
 }
-export const helpDescription = 'Shows you status information about the bot';
+export const helpDescription = 'Shows you information about the bot';
