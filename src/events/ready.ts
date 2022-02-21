d4cb1cca1d2b64d6cc2deeeebcd3f005111c56e8
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2022-02-21 09:23:52 +0100

    Automatic deployment of commands

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,7 +1,33 @@
 import type { Client } from 'discord.js';
+import { readdirSync } from 'fs';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import type { Command } from '../interfaces/command';
 
 export const name = 'ready';
 export const once = true;
-export function execute(client: Client): void {
-  console.log(`Ready!\nLogged in as ${client.user?.tag}`);
+export async function execute(client: Client) {
+  await registererCommands().then(() => console.log(`Ready!\nLogged in as ${client.user?.tag}`));
+
+  // Automatic deployment of commands
+  async function registererCommands() {
+    const commands: unknown[] = [];
+    const commandFiles = readdirSync('dist/commands').filter((file) => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+      commands.push(
+        await import(`../commands/${file}`).then((command: Command) => {
+          console.log(`Added command: ${command.data.name}`);
+          return command.data.toJSON();
+        })
+      );
+    }
+
+    const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN ?? '');
+    console.log(`Registering ${commands.length} commands...`);
+    await rest
+      .put(Routes.applicationCommands(process.env.CLIENT_ID ?? ''), { body: commands })
+      .then(() => console.log(`Successfully registered ${commands.length} application commands.`))
+      .catch(console.error);
+  }
 }
