80abc76e628c5cb3cea7b3c4f7a98b04d1a2e034
VIKTORVAV99 <30777521+VIKTORVAV99@users.noreply.github.com>
2022-02-25 08:32:33 +0100

    Error check

--- src/commands/cmProps.ts
+++ src/commands/cmProps.ts
@@ -18,6 +18,7 @@ let props: {
   address: string;
 }[] = [];
 let fetchedAt: Date = new Date();
+let apiError = false;
 
 /**
  * Get properties from the monKey Miles API and format them.
@@ -48,6 +49,7 @@ async function fetchProperties() {
         };
       });
   } catch {
+    apiError = true;
     console.warn('There was a problem converting the API response to the property object structure!');
   }
 }
@@ -55,9 +57,9 @@ async function fetchProperties() {
 await fetchProperties();
 setInterval(fetchProperties, 8.62e7);
 
-/**
- * ==== Command code ====
- */
+//
+// ==== Command code ====
+//
 // ToDo: Clean up and make the code more dynamic.
 export const data = new SlashCommandBuilder()
   .setName('cmprops')
@@ -75,46 +77,54 @@ export async function execute(interaction: CommandInteraction) {
   const city = interaction.options.getString('city');
   const responseArray1: string[] = [];
   const responseArray2: string[] = [];
-  props.forEach((data) => {
-    const property = hyperlink(data.address, `https://play.upland.me/?prop_id=${data.id}`);
+  let embedResponse: MessageEmbedOptions = {};
+  if (!apiError) {
+    props.forEach((data) => {
+      const property = hyperlink(data.address, `https://play.upland.me/?prop_id=${data.id}`);
 
-    if (data.city === city && responseArray1.join('\n').length + property.length <= 1024) {
-      responseArray1.push(property);
-    } else if (data.city === city && responseArray2.join('\n').length + property.length <= 1024) {
-      responseArray2.push(property);
+      if (data.city === city && responseArray1.join('\n').length + property.length <= 1024) {
+        responseArray1.push(property);
+      } else if (data.city === city && responseArray2.join('\n').length + property.length <= 1024) {
+        responseArray2.push(property);
+      }
+    });
+
+    let responseFields = [];
+    if (responseArray2.length > 0) {
+      responseFields = [
+        {
+          name: 'Properties:',
+          value: responseArray1.join('\n'),
+        },
+        {
+          name: 'More properties:',
+          value: responseArray2.join('\n'),
+        },
+      ];
+    } else {
+      responseFields = [
+        {
+          name: 'Properties:',
+          value: responseArray1.join('\n'),
+        },
+      ];
     }
-  });
 
-  let responseFields = [];
-  if (responseArray2.length > 0) {
-    responseFields = [
-      {
-        name: 'Properties:',
-        value: responseArray1.join('\n'),
+    embedResponse = {
+      color: 0xfbdd11,
+      title: `CryptomonKey properties`,
+      description: `The CryptomonKeys properties in ${city}`,
+      fields: responseFields,
+      footer: {
+        text: `Updated at: ${fetchedAt.toUTCString()}\nSpecial thanks to CryptomonKeys and Green for the data.`,
       },
-      {
-        name: 'More properties:',
-        value: responseArray2.join('\n'),
-      },
-    ];
-  } else {
-    responseFields = [
-      {
-        name: 'Properties:',
-        value: responseArray1.join('\n'),
-      },
-    ];
+    };
+  } else if (apiError) {
+    embedResponse = {
+      title: 'Error',
+      description: 'There was a error fetching the data.',
+    };
   }
-
-  const embedResponse: MessageEmbedOptions = {
-    color: 0xfbdd11,
-    title: `CryptomonKey properties`,
-    description: `The CryptomonKeys properties in ${city}`,
-    fields: responseFields,
-    footer: {
-      text: `Updated at: ${fetchedAt.toUTCString()}\nSpecial thanks to CryptomonKeys and Green for the data.`,
-    },
-  };
   await interaction.reply({ embeds: [embedResponse] });
 }
 
